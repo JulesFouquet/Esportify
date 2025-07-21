85fce6de528de4c7b40eef58626600b1e3534c3e
Fouquet Jules <julesfouquetprowd@gmail.com>
2025-07-21 20:36:11 +0200

    Fixe login pour Heroku

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,23 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class SecurityController extends AbstractController
 {
     // Route page de connexion
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-    
+        $session = $request->getSession();
+        if (!$session->isStarted()) {
+            $session->start();
+        }
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
+
     // Route pour la déconnexion
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    // Route temporaire pour tester le CSRF
+    #[Route('/csrf-test', name: 'csrf_test')]
+    public function csrfTest(CsrfTokenManagerInterface $csrfTokenManager): Response
+    {
+        $token = $csrfTokenManager->getToken('authenticate')->getValue();
+
+        return new Response('Token CSRF généré : ' . $token);
     }
 }
